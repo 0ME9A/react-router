787a10faffb19b4efffb564d57f0bc06c2419266
Baliram <heyome9a@gmail.com>
2022-10-13 22:00:17 +0530

    v-1.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Contact from './components/Contact';
+import About from './components/About';
+import Error from './components/Error';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav />
+        <Routes>
+          <Route path="https://react-router-six.netlify.app/" element={<Home />} >
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+
+
+        </Routes>
+
       </header>
     </div>
   );
